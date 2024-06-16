d78c02bbaf0dacd2982c410d3691a956fe2a159b
Zach Stout <stout.zachary@gmail.com>
2024-06-15 23:50:49 -0600

    register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,6 +2,12 @@ export default function RegisterPage() {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <div>Register My Athlete</div>
+      <div>Sign Up</div>
+      <div>Log In</div>
+      <div>Forgot Password</div>
+      <div>Reset Password</div>
+      <div>Change Password</div>
+      <div>Delete Account</div>
     </div>
   );
 }

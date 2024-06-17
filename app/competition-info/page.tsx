9a9454d89d9134dd14cfc904a3b7b0b04df14ec6
Zach Stout <stout.zachary@gmail.com>
2024-06-17 12:00:00 -0600

    added in comp info

--- app/competition-info/page.tsx
+++ app/competition-info/page.tsx
@@ -1,7 +1,272 @@
 export default function CompetitionInfoPage() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <div>Competition Info</div>
+    <div className="container mx-auto">
+      <div className="flex flex-wrap">
+        <div className="w-full md:w-1/2">
+          <div className="h-full rounded-md border border-gray-300 bg-gray-100 p-6">
+            <div className="mb-4"></div>
+            <div className="flex flex-col">
+              <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900">
+                Mustang classic at Herriman High School
+              </dt>
+              <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-600">
+                <p className="flex-auto font-bold">
+                  Solo Competition - Friday, Dec 1st
+                </p>
+                <ul className="list-disc pl-6">
+                  <li>Cate Arnold - 4:30pm</li>
+                  <li>Marina Allen - 9:15pm</li>
+                  <li>Kaitlyn Sonntag - 10:30pm</li>
+                  <li>Awards - 10:40pm</li>
+                </ul>
+
+                <p className="flex-auto font-bold">
+                  Team Competition - Saturday, Dec 2nd
+                </p>
+                <ul className="list-disc pl-6">
+                  <li>Military - 11:20am</li>
+                  <li>Dance - 1:50pm</li>
+                  <li>Show - 3:25pm</li>
+                  <li>Drill Down & Awards - 3:50pm</li>
+                </ul>
+
+                <p className="mt-6">
+                  <p>
+                    <a
+                      target="_blank"
+                      href="https://gofan.co/event/1214913?schoolId=UT86863&utm_source=GoFan&utm_medium=qrcode&utm_campaign=HQEventLink"
+                      className="text-sm font-semibold leading-6 text-primary"
+                    >
+                      Purchase solo competition tickets{" "}
+                      <span aria-hidden="true">→</span>
+                    </a>
+                  </p>
+                  <p>
+                    <a
+                      target="_blank"
+                      href="https://gofan.co/event/1179901?schoolId=UT86863&utm_source=GoFan&utm_medium=qrcode&utm_campaign=HQEventLink"
+                      className="text-sm font-semibold leading-6 text-primary"
+                    >
+                      Purchase team competition tickets{" "}
+                      <span aria-hidden="true">→</span>
+                    </a>
+                  </p>
+                  <p className="mt-3 flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900">
+                    Admission for Friday and Saturday will be $10 per person
+                    with 5 and under free.
+                  </p>
+                </p>
+              </dd>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full md:w-1/2">
+          <div className="h-full rounded-md border border-gray-300 bg-gray-100 p-6 md:border">
+            <div className="mb-4"></div>
+            <div className="flex flex-col">
+              <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900">
+                Showcase at UVU at the UCCU Center
+              </dt>
+              <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-600">
+                <p className="flex-auto font-bold">
+                  Solo competition - Friday, Dec 15th
+                </p>
+                <ul className="list-disc pl-6">
+                  <li>Cate Arnold - 2:45pm</li>
+                  <li>Kaitlyn Sonntag - 8:00pm</li>
+                </ul>
+                <p>
+                  <a
+                    target="_blank"
+                    href="https://docs.google.com/spreadsheets/d/1vaFqwDtl723XjR-jCnY6oPuuBF3q5dbc_TWV3sEAePo/edit#gid=0"
+                    className="text-sm font-semibold leading-6 text-primary"
+                  >
+                    SOLO SCHEDULE <span aria-hidden="true">→</span>
+                  </a>
+                </p>
+                <p className="flex-auto font-bold">
+                  Team competition - Saturday, Dec 16th
+                </p>
+                <ul className="list-disc pl-6">
+                  <li>Military - 8:50am</li>
+                  <li>Dance - 10:45am</li>
+                  <li>Show - 11:15am</li>
+                  <li>Drill Down & Awards - 12:00pm</li>
+                </ul>
+                <p>
+                  <a
+                    target="_blank"
+                    href="https://docs.google.com/spreadsheets/d/14xQLVcY2QwSGE0oPvwLrpAqCMjweFzXnSdRNoBAU_P0/edit#gid=0"
+                    className="text-sm font-semibold leading-6 text-primary"
+                  >
+                    TEAM SCHEDULE <span aria-hidden="true">→</span>
+                  </a>
+                </p>
+                <p className="mt-5 flex-auto">
+                  MUSIC: Soloists, email your music to{" "}
+                  <span className="text-accent">showcaseuvu@gmail.com</span> &
+                  label it with your school name and category.
+                </p>
+
+                <p className="mt-5">
+                  <a
+                    target="_blank"
+                    href="https://bit.ly/46v3sIc"
+                    className="text-sm font-semibold leading-6 text-primary"
+                  >
+                    Purchase solo competition tickets{" "}
+                    <span aria-hidden="true">→</span>
+                  </a>
+                </p>
+                <p>
+                  <a
+                    target="_blank"
+                    href="https://bit.ly/49YUJRC "
+                    className="text-sm font-semibold leading-6 text-primary"
+                  >
+                    Purchase 5A/6A team competition tickets{" "}
+                    <span aria-hidden="true">→</span>
+                  </a>
+                </p>
+                <p className="mt-3 flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900">
+                  Ticket Prices are $10.00 for all ages.  A service fee is added
+                  to all digital ticketing.
+                </p>
+              </dd>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full md:w-1/2">
+          <div className="h-full rounded-md border border-gray-300 bg-gray-100 p-6">
+            <div className="mb-4"></div>
+            <div className="flex flex-col">
+              <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900">
+                Bountiful Invitational at Bountiful High School
+              </dt>
+              <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-600">
+                <p className="flex-auto">Solo competition - Friday, Jan 5th</p>
+                <p className="flex-auto font-bold">
+                  Team competition - Saturday, Jan 6th @ 3:30pm
+                </p>
+                <ul className="list-disc pl-6">
+                  <li>Military - 4:30pm</li>
+                  <li>Dance - 5:40pm</li>
+                  <li>Show - 7:00pm</li>
+                  <li>Drill Down & Awards - 7:30pm</li>
+                </ul>
+                <p className="mt-6">
+                  <p className="mt-3 flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900">
+                    Schedule will be posted here as soon as we get it. Tickets
+                    for this competition will be done on the GoFan app, so
+                    download that soon because tickets will go on sale shortly.
+                  </p>
+                  <a
+                    target="_blank"
+                    href="https://docs.google.com/forms/d/e/1FAIpQLSd8TwWjjCe37VBfkwAacob3L2gRW11tNp1_SE-AJkXyiWypAw/viewform"
+                    className="mt-5 text-sm font-semibold leading-6 text-primary"
+                  >
+                    Register for Bountiful Invitational{" "}
+                    <span aria-hidden="true">→</span>
+                  </a>
+                </p>
+                <p className="">
+                  <a
+                    target="_blank"
+                    href="https://dsdgive.net/event/3483"
+                    className="text-sm font-semibold leading-6 text-primary"
+                  >
+                    Bountiful Invitational payment link{" "}
+                    <span aria-hidden="true">→</span>
+                  </a>
+                </p>
+              </dd>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full md:w-1/2">
+          <div className="h-full rounded-md border border-gray-300 bg-gray-100 p-6">
+            <div className="mb-4"></div>
+            <div className="flex flex-col">
+              <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900">
+                Region
+              </dt>
+              <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-600">
+                <p className="flex-auto">
+                  <b>Wednesday, January 17th 4:00pm</b> at American Fork High
+                  School
+                </p>
+                <p className="mt-5 flex-auto">
+                  Each school will be allowed to sell up to 160 pre-sold
+                  tickets. Link to those ticket sales will be sent out January
+                  8th. General Admission will be sold the day of the event and
+                  will be first come, first serve. They WILL sell out.
+                </p>
+              </dd>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full md:w-1/2">
+          <div className="h-full rounded-md border-gray-300 bg-gray-100 p-6 md:border">
+            <div className="mb-4"></div>
+            <div className="flex flex-col">
+              <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900">
+                State Quarter-Finals
+              </dt>
+              <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-600">
+                <p className="flex-auto">January 30th at UVU</p>
+                <p className="flex-auto">More info to come...</p>
+              </dd>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full md:w-1/2">
+          <div className="h-full rounded-md border border-gray-300 bg-gray-100 p-6">
+            <div className="mb-4"></div>
+            <div className="flex flex-col">
+              <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900">
+                State Semi-Finals
+              </dt>
+              <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-600">
+                <p className="flex-auto">February 1st at UVU</p>
+                <p className="flex-auto">More info to come...</p>
+              </dd>
+            </div>
+          </div>
+        </div>
+        <div className="w-full md:w-1/2">
+          <div className="h-full rounded-md border border-gray-300 bg-gray-100 p-6">
+            <div className="mb-4"></div>
+            <div className="flex flex-col">
+              <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900">
+                State Finals
+              </dt>
+              <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-600">
+                <p className="flex-auto">February 2nd at UVU</p>
+                <p className="flex-auto">More info to come...</p>
+              </dd>
+            </div>
+          </div>
+        </div>
+        <div className="w-full md:w-1/2">
+          <div className="h-full rounded-md border border-gray-300 bg-gray-100 p-6">
+            <div className="mb-4"></div>
+            <div className="flex flex-col">
+              <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900">
+                Nationals
+              </dt>
+              <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-600">
+                <p className="flex-auto">February 8-11th in Orlando, FL</p>
+                <p className="flex-auto">More info to come...</p>
+              </dd>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

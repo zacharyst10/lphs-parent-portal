821b6794c2f951125f906c274acc15128bb67651
Zach Stout <stout.zachary@gmail.com>
2024-06-25 12:00:00 -0600

    updates

--- app/nationals/page.tsx
+++ app/nationals/page.tsx
@@ -2,6 +2,18 @@ export default function NationalsPage() {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <div>Nationals</div>
+      <div className="flex flex-col gap-y-4">
+        <div className="flex flex-col gap-y-4">
+          <div className="flex flex-col gap-y-4">
+            <h1 className="text-2xl font-bold">Nationals</h1>
+            <p className="text-lg">
+              The Nationals are the final competition of the season. The
+              Nationals are held in Orlando, Florida, USA. The Nationals are
+              held on February 8-11th.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
